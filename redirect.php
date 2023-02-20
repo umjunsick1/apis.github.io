376011963a636a05c824084db908e222a01e84ea
User-7777 <78833731+User-7777@users.noreply.github.com>
2023-02-20 10:30:41 +0900

    Create redirect.php

---
+++ redirect.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Redirecting...</title>
+	<meta http-equiv="refresh" content="0; url=<?php echo $_GET['url']; ?>">
+</head>
+<body>
+	<p>Redirecting to <?php echo $_GET['url']; ?>...</p>
+</body>
+</html>
