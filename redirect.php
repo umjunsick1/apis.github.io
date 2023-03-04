f6ad36d38b267e7254b8bee2be221df88955a29f
umjunsick <78833731+umjunsick1@users.noreply.github.com>
2023-03-04 10:46:20 +0900

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -5,3 +5,18 @@ if(isset($_GET['redirect'])){
   exit();
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>리다이렉트 페이지</title>
+</head>
+<body>
+  <h1>리다이렉트 페이지</h1>
+  <form action="" method="get">
+    <label>리다이렉트할 URL을 입력하세요:</label>
+    <input type="text" name="redirect">
+    <button type="submit">리다이렉트</button>
+  </form>
+</body>
+</html>
