7f710f25b2f7bac10945649ff744fbcef7d2f96d
User-7777 <78833731+User-7777@users.noreply.github.com>
2023-03-04 10:27:31 +0900

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Redirecting...</title>
-	<meta http-equiv="refresh" content="0; url=<?php echo $_GET['url']; ?>">
-</head>
-<body>
-	<p>Redirecting to <?php echo $_GET['url']; ?>...</p>
-</body>
-</html>
+<?php
+if(isset($_GET['redirect'])){
+  $url = $_GET['redirect'];
+  header("Location: $url");
+  exit();
+}
+?>
